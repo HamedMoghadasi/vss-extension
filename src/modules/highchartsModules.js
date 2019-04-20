f14824130358cf926403b54af4247ed255e6b927
Hamed Moghadasi <h4lmed@gmail.com>
2019-04-20 16:32:00 +0430

    :building_construction: Making architectural changes.

---
+++ src/modules/highchartsModules.js
@@ -0,0 +1,78 @@
+var highchartsModule = {
+  draw: function(diagramData) {
+    Highcharts.chart("diagramContainer", {
+      chart: {
+        height: 150,
+        type: "spline"
+      },
+      title: false,
+      subtitle: false,
+      xAxis: {
+        type: "datetime",
+        labels: {
+          formatter: function() {
+            return Highcharts.dateFormat("%d %b %Y", this.value);
+          }
+        },
+        title: {
+          text: null
+        }
+      },
+      yAxis: {
+        tickInterval: 1,
+        visible: false,
+        title: {
+          text: null
+        },
+        min: 0
+      },
+      time: {
+        dateformat: "%Y-%m-%d"
+      },
+      tooltip: {
+        headerFormat: "<b>{series.name}</b><br>",
+        pointFormat: "{point.x:%Y-%m-%d}"
+      },
+
+      plotOptions: {
+        spline: {
+          marker: {
+            enabled: true
+          }
+        },
+        series: {
+          lineWidth: 5
+        }
+      },
+      legend: {
+        enabled: true,
+        align: "center",
+        verticalAlign: "top",
+        y: 0,
+        floating: true
+      },
+      exporting: { enabled: false },
+      colors: ["#ef5661", "#6CF"],
+      credits: {
+        enabled: false
+      },
+
+      series: [
+        {
+          name: "Lead Time",
+          data: [
+            [new Date(diagramData.createdOn).getTime(), 0.2],
+            [new Date(diagramData.closedOn).getTime(), 0.2]
+          ]
+        },
+        {
+          name: "Cycle Time",
+          data: [
+            [new Date(diagramData.startedOn).getTime(), 0.4],
+            [new Date(diagramData.closedOn).getTime(), 0.4]
+          ]
+        }
+      ]
+    });
+  }
+};
